2cc4a5c1509a0e7b91eb990377ebae894bfe31d8
webster <michael@webster.fyi>
2025-03-26 20:19:35 -0700

    Move to jsonl for response test case parsing.
    Instead of regexing the CSV columns, load query, response, tests from
    jsonl file.
    
    Also includes some minor cleanup of print and debug info.

--- test_lmunit.py
+++ test_lmunit.py
@@ -1,7 +1,6 @@
 import requests
 from openai import OpenAI
 import pandas as pd
-import re
 import time
 import os
 
@@ -42,11 +41,7 @@ def lmunit(query: str, response: str, unit_test: str):
 
     unit_test_score = requests.post(lmunit_api_url, json=params, headers=headers)
 
-    try:
-        print(unit_test_score.json())
-        return unit_test_score.json()["score"]
-    except:
-        print(unit_test_score.json())
+    return unit_test_score.json()["score"]
 
 
 def test_evalset():
@@ -55,37 +50,24 @@ def test_evalset():
     Alternatively, you could use the @pytest.mark.parametrize method.
     """
 
-    # Read the CSV file
-    df = pd.read_csv("evalset.csv")
-
-    # Read each column individually
-    queries = df["query"].tolist()
-    responses = df["response"].tolist()
-    unit_tests = df["unit_tests"].tolist()
+    df = pd.read_json("evalset.jsonl", lines=True)
 
     # run unit tests on each column
-    for query, response, unit_test_row_string in zip(queries, responses, unit_tests):
-        print(query)
-        print(response)
-        unit_test_row_list = re.findall(
-            r"“([^”]*)”", unit_test_row_string
-        )  # there has to be a better way
-        unit_test_full_list = global_unit_tests + unit_test_row_list
-        for unit_test in unit_test_full_list:
+    for idx, row in df.iterrows():
+        query = row["query"]
+        response = row["response"]
+
+        # LMUnit tests are defined as natural language queries that are sent to the LMUnit model.
+        # In this dataset we have multiple unit tests associated with a query.
+        # You may think of this like a suite of tests that apply to each query and response pairs.
+        unit_tests = row["unit_tests"]
+        for unit_test in unit_tests:
             print(unit_test)
             unit_test_score = lmunit(query, response, unit_test)
             assert unit_test_score >= 1
+            # wait to avoid rate limiting in the API
             time.sleep(1)
-        print("-----")
 
 
-if __name__ == "__main__":  # TODO: Remove all of this
-
-    query = "What is the capital of France?"
-    response = "Capital of France is Paris."
-    unit_test = "Does the response correctly identify the capital of France?"
-
-    # unit_test_score = lmunit(query, response, unit_test)
-    # print(unit_test_score)
-
+if __name__ == "__main__":
     test_evalset()
