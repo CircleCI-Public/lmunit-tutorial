c71aa0785b88c8d5987b53b4f58fea6cf60c7c8e
michael-webster <michaelwebster@circleci.com>
2025-03-27 09:06:34 -0700

    Adjust readme and instructions for initial setup.
    * The anyio dependency requires python 3.0, but the CI environment was using 3.8.
    Changing the container version for the build to get a working pipeline.
    * Move to jsonl for response test case parsing instead of CSV
    * Also includes some minor cleanup of print and debug info.
    * Update README.md with more context.
    
    Co-authored-by: webster <michael@webster.fyi>
    Co-authored-by: Ryan E. Hamilton <104376313+ryan-circleci@users.noreply.github.com>

--- test_lmunit.py
+++ test_lmunit.py
@@ -1,11 +1,13 @@
 import requests
 from openai import OpenAI
 import pandas as pd
-import re
 import time
+import os
 
-# Set your OpenAI API key  
-client = OpenAI(api_key='<insert your API key here>')
+openai_api_key = os.environ.get("OPENAI_API_KEY")
+lmunit_api_key = os.environ.get("LMUNIT_API_KEY")
+# Set your OpenAI API key
+client = OpenAI(api_key=openai_api_key)
 
 # Global unit tests apply to all queries in your evaluation set
 global_unit_tests = ["Is the response a complete sentence?"]
@@ -19,7 +21,7 @@ def generate_llm_output(query: str):
     response = client.chat.completions.create(
         model="gpt-4-turbo-preview",
         messages=[{"role": "user", "content": query}],
-        temperature=0
+        temperature=0,
     )
 
     return response.choices[0].message.content
@@ -30,55 +32,42 @@ def lmunit(query: str, response: str, unit_test: str):
     Use LMUnit to evaluate the response against the given unit test.
     """
     params = {"query": query, "response": response, "unit_test": unit_test}
-    headers = {"accept": "application/json",
-               "Authorization": "Bearer key-WYGTfgkbXVj1iz8mYRobu80YyLNUhDWXnmzV-GNVvVHC5mWm8",
-               "Content-Type": "application/json"}
+    headers = {
+        "accept": "application/json",
+        "Authorization": f"Bearer {lmunit_api_key}",
+        "Content-Type": "application/json",
+    }
     lmunit_api_url = "https://api.contextual.ai/v1/lmunit"
 
     unit_test_score = requests.post(lmunit_api_url, json=params, headers=headers)
-    
-    try:
-        print(unit_test_score.json())
-        return unit_test_score.json()['score']
-    except:
-        print(unit_test_score.json())
+
+    return unit_test_score.json()["score"]
 
 
 def test_evalset():
     """
-    This method retrieves queries, responses, and unit tests from an evalset.csv file. Then, for each query, it runs each unit test. 
-    Alternatively, you could use the @pytest.mark.parametrize method. 
+    This method retrieves queries, responses, and unit tests from an evalset.csv file. Then, for each query, it runs each unit test.
+    Alternatively, you could use the @pytest.mark.parametrize method.
     """
 
-    # Read the CSV file
-    df = pd.read_csv('evalset.csv')
-
-    # Read each column individually
-    queries = df['query'].tolist()
-    responses = df['response'].tolist()
-    unit_tests = df['unit_tests'].tolist()
+    df = pd.read_json("evalset.jsonl", lines=True)
 
     # run unit tests on each column
-    for query, response, unit_test_row_string in zip(queries, responses, unit_tests):
-        print(query)
-        print(response)
-        unit_test_row_list =  re.findall(r'“([^”]*)”', unit_test_row_string)  # there has to be a better way
-        unit_test_full_list = global_unit_tests + unit_test_row_list
-        for unit_test in unit_test_full_list:
+    for idx, row in df.iterrows():
+        query = row["query"]
+        response = row["response"]
+
+        # LMUnit tests are defined as natural language queries that are sent to the LMUnit model.
+        # In this dataset we have multiple unit tests associated with a query.
+        # You may think of this like a suite of tests that apply to each query and response pairs.
+        unit_tests = row["unit_tests"]
+        for unit_test in unit_tests:
             print(unit_test)
             unit_test_score = lmunit(query, response, unit_test)
             assert unit_test_score >= 1
+            # wait to avoid rate limiting in the API
             time.sleep(1)
-        print('-----')
 
 
-if __name__ == "__main__":  # TODO: Remove all of this
-
-    query = "What is the capital of France?"
-    response = "Capital of France is Paris." 
-    unit_test = "Does the response correctly identify the capital of France?"
-
-    # unit_test_score = lmunit(query, response, unit_test)
-    # print(unit_test_score)
-
+if __name__ == "__main__":
     test_evalset()
