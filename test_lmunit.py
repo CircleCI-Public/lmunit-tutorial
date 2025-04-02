c3093ec9b6d6858e852804a799a2514b9f1387af
Ishan Sinha <ishan.sinha@contextual.ai>
2025-04-02 16:14:30 -0700

    Use Grounded Language Model with Contextual Python SDK (#7)
    * Use Contextual Python SDK, add knowledge to evalset and use Grounded Language Model, update unit tests so that they are answerable with the provided knowledge
    
    ---------
    
    Co-authored-by: michael-webster <michaelwebster@circleci.com>

--- test_lmunit.py
+++ test_lmunit.py
@@ -1,53 +1,45 @@
-import requests
 import pandas as pd
 import time
 import os
+from contextual import ContextualAI
 
-lmunit_api_key = os.environ.get("LMUNIT_API_KEY")
+# Set up Contextual SDK
+contextual_api_key = os.environ.get("CONTEXTUAL_API_KEY")
+client = ContextualAI(api_key = contextual_api_key, base_url = "https://api.contextual.ai/v1")
+
 
 # Global unit tests apply to all queries in your evaluation set
-global_unit_tests = ["Is the response a complete sentence?"]
-
-
-def lmunit(query: str, response: str, unit_test: str):
-    """
-    Use LMUnit to evaluate the response against the given unit test.
-    """
-
-    params = {"query": query, "response": response, "unit_test": unit_test}
-    headers = {
-        "accept": "application/json",
-        "Authorization": f"Bearer {lmunit_api_key}",
-        "Content-Type": "application/json",
-    }
-    lmunit_api_url = "https://api.contextual.ai/v1/lmunit"
-
-    unit_test_score = requests.post(lmunit_api_url, json=params, headers=headers)
-
-    return unit_test_score.json()["score"]
+global_unit_tests = ["Does the response directly address the prompt’s query or topic?"]
 
 
 def test_evalset():
     """
-    This method retrieves queries, responses, and unit tests from an evalset.jsonl file. Then, for each query, it runs each unit test.
+    This method retrieves queries and unit tests from an evalset.jsonl file. 
+    For each query, it generates a response using Contextual's Grounded Language Model. 
+    Then, it uses LMUnit to evaluate each response against each relevant unit test. 
     Alternatively, you could use the @pytest.mark.parametrize method.
     """
 
     df = pd.read_json("evalset.jsonl", lines=True)
 
-    # run unit tests on each column
+    # run unit tests for each row in the evalset
     for idx, row in df.iterrows():
         query = row["query"]
-        response = row["response"]  # In practice, you would sample your LLM or agent to get a response.
+        knowledge = row["knowledge"]  # This is the knowledge that Contextual's Grounded Language Model will use to generate a response.
+        response = client.generate.create(
+            model="v1",
+            messages=[{"role": "user", "content": query}],
+            knowledge=[knowledge],      
+        ).response
 
         # LMUnit tests are defined as natural language queries that are sent to the LMUnit model.
         # In this dataset we have multiple unit tests associated with a query.
         # You may think of this like a suite of tests that apply to each query and response pair. 
-        unit_tests = row["unit_tests"]
+        unit_tests = row["unit_tests"] + global_unit_tests
 
         for unit_test in unit_tests:
-            unit_test_score = lmunit(query, response, unit_test)
-            assert unit_test_score >= 3.0, f"Failed unit test: \"{unit_test}\" for query: \"{query}\""  #  Some tests will fail by design. Change the threshold to 1.0 for all tests to pass.
+            unit_test_score = client.lmunit.create(query=query, response=response, unit_test=unit_test).score
+            assert unit_test_score >= 3.0, f"Failed unit test: \"{unit_test}\" for query: \"{query}\""  # score of 3.0 is passing
             time.sleep(1)  # wait to avoid rate limiting in the API
 
 
